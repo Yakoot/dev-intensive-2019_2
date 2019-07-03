efc248dd1e3d7291b549dde7d202ebc8e6c1f7bf
Anton Mamkin <mail@tbrd.ru>
2019-07-04 02:48:45 +0300

    another test fix

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -2,9 +2,9 @@ package ru.skillbranch.devintensive.extensions
 
 
 fun String.truncate(lastIndex: Int = 16): String {
-    val truncatedStr = this.substring(0, lastIndex)
-    val trimmedStr = truncatedStr.trim()
-    return trimmedStr + if (this.length != truncatedStr.length) "..." else ""
+    val trimmed = this.trim()
+    val truncated = if (trimmed.length < lastIndex) trimmed else trimmed.substring(0..lastIndex).trim()
+    return truncated + if (truncated.length != trimmed.length) "..." else ""
 }
 
 fun String.stripHtml(): String {
